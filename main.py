6a8b73e7676a78a6a986bad373c88514ba9d64ca
Caiotullio <caio_13_16@hotmail.com>
2022-02-17 23:27:53 -0300

    pessoa

---
+++ main.py
@@ -0,0 +1,15 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+#p1.comer('maça')
+#p1.parar_comer()
+#p1.parar_comer()
+#p1.comer('maça')
+
+p1.comer('maça')
+p1.falar('POO')
+p1.parar_comer()
+p1.falar('POO')
+p1.comer('alimento')
+p1.parar_falar()
+p1.falar('assunto')
